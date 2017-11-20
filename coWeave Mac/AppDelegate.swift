8e802b833bfdd448afd5e60127cc7888bcfd3b0a
Benoît Frisch <benoit@fresh.lu>
2017-11-20 11:08:39 +0100

    Initial Commit

---
+++ coWeave Mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  coWeave Mac
+//
+//  Created by Benoît Frisch on 20/11/2017.
+//  Copyright © 2017 Benoît Frisch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ coWeave Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  coWeave Mac
+//
+//  Created by Benoît Frisch on 20/11/2017.
+//  Copyright © 2017 Benoît Frisch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
